b55e06798330366e7fad0dddb4aa664f15ddbc4a
Lauren <Lauren@DESKTOP-QVKD4VF>
2016-09-08 01:41:27 -0500

    Add project files.

---
+++ StudyStation/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using StudyStation.Infrastructure;
+
+namespace StudyStation.App_Start
+{
+    public class FilterConfig
+    {
+        public static void Configure(System.Web.Mvc.GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
