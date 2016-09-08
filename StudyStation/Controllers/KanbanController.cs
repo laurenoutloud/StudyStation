241ebb2f78690e12f38c7533d8bb6d4d53e776ef
Lauren <lauren.finley01@gmail.com>
2016-09-08 03:52:02 -0500

    Created skeleton of application by setting up all of the basic views, controllers, and a basic navigation bar in the shared layout view. (*Note, all views lack content and all controllers lack functionality at this stage. Also, views will be organized more efficiently later without the excess use of folders.)

---
+++ StudyStation/Controllers/KanbanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudyStation.Controllers
+{
+    public class KanbanController : Controller
+    {
+        // GET: Kanban
+        public ActionResult Kanban()
+        {
+            return View();
+        }
+    }
+}
